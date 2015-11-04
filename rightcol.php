c64682b561041c427ad31ad69ebc20ed2ef174ee
osnaraus <osnaraus@gmail.com>
2015-11-04 10:29:55 -0300

    new

---
+++ rightcol.php
@@ -0,0 +1,20 @@
+<div class="col m2 l2 hide-on-small-only col-der">
+    <div class="row">
+        <div class="col m12 l12">
+            <div class="toc-wrapper">
+
+                  <div class="section table-of-contents center">
+                    <a id="id-radio1" class="radio-online"><img src="publi/envivo.gif" style="width:100%" alt="" class="responsive-img"></a>									
+                    <img src="publi/300x300-arba.gif" style="width:100%" alt="" class="responsive-img">
+                    <img src="publi/gba.jpg" style="width:100%" alt="" class="responsive-img">
+
+                    <!--<img src="publi/mavi.gif" style="width:100%" alt="" class="responsive-img"> -->
+                    <!--<img src="publi/salvador.jpg" style="width:100%" alt="" class="responsive-img"> -->
+                     <!-- <img src="publi/911.jpg" style="width:80%" alt="" class="responsive-img">-->
+
+                  </div>
+
+            </div>
+        </div>
+    </div>
+</div>
